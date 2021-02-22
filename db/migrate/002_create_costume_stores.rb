0c83bcfca35dbe2619894cd92c90a228608c92fd
Samuel DeSantis <samuel.i.desantis@gmail.com>
2021-02-22 03:02:08 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,16 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :employees
+      t.bool :still_in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
+
+      t.timestamp null: false
+    end
+  end
+end
